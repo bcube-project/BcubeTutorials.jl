0654d5f86e736f26b4824dae013de8c90eebccc4
Maxime Bouyges <maxime.bouyges@gmail.com>
2023-11-23 13:29:03 +0100

    linear_elasticity

--- src/example/linear_elasticity/linear_elasticity.jl
+++ src/example/linear_elasticity/linear_elasticity.jl
@@ -15,8 +15,8 @@ const degree = 1 # FunctionSpace degree
 const degquad = 2 * degree + 1
 
 # Input and output paths
-const outputpath = dir * "../myout/elasticity/"
-const meshpath = dir * "../input/mesh/domainElast_tri.msh"
+const outputpath = dir * "../../myout/elasticity/"
+const meshpath = dir * "../../../input/mesh/domainElast_tri.msh"
 
 # Time stepping scheme params
 const α = 0.05
