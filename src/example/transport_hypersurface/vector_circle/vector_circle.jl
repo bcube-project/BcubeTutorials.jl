1c483ead89547fec3488142806add6d13cfedb35
Maxime Bouyges <maxime.bouyges@gmail.com>
2024-02-05 17:50:31 +0100

    vector transport ok

--- src/example/transport_hypersurface/vector_circle/vector_circle.jl
+++ src/example/transport_hypersurface/vector_circle/vector_circle.jl
@@ -21,7 +21,9 @@ function plot_solution(u, mesh, degree, xplot, yplot, xnodes, ynodes)
             aspect_ratio = :equal,
             primary = false,
         )
-    scatter!(xplot, yplot; marker_z = uplot, label = "u")
+    quiver!(xplot, yplot; quiver = (uplot[:, 1], uplot[:, 2]), label = "u")
+    xlims!(-2, 2)
+    ylims!(-2, 2)
 
     return plt
 end
@@ -30,11 +32,11 @@ end
 const degree = 0
 const nθ = 10
 const radius = 1.0
-const nite = 20
+const nite = 18
 const CFL = 1
 const C = 1.0 # velocity norm
 
-out_dir = joinpath(@__DIR__, "../../../../myout/sdg_transport_circle")
+out_dir = joinpath(@__DIR__, "../../../../myout/sdg_transport_vec_circle")
 mkpath(out_dir)
 
 # Mesh
@@ -45,55 +47,85 @@ dΓ = Measure(Γ, 2 * degree + 1)
 nΓ = get_face_normals(Γ)
 
 # Transport velocity
-_c = PhysicalFunction(x -> C * SA[-x[2], x[1]] / radius)
+c = PhysicalFunction(x -> -C * SA[-x[2], x[1]] / radius)
 P = Bcube.TangentialProjector()
-c = -C * (P * _c) / mynorm(P * _c)
-# c = _c
-# println("wrong velocity for debug")
-# c = SA[1, 0]
 
 # FESpace
 fs = FunctionSpace(:Lagrange, degree)
-U = TrialFESpace(fs, mesh, :discontinuous)
+U = TrialFESpace(fs, mesh, :discontinuous; size = 2)
 V = TestFESpace(U)
 
 # FEFunction and "boundary / source" condition
 u = FEFunction(U)
-u.dofValues[1] = 1.0
+
+# Initial condition
+_u0 = PhysicalFunction(x -> begin
+    θ = atan(x[2], x[1])
+    if 0 <= θ <= 2π / nθ
+        return radius * SA[-x[2], x[1]]
+    else
+        return SA[0, 0]
+    end
+end)
+projection_l2!(u, _u0, mesh)
+@show u.dofValues
 
 # Forms
 m(u, v) = ∫(u ⋅ v)dΩ # Mass matrix
-l_Ω(v) = ∫(u * (c ⋅ ∇(v)))dΩ # Volumic convective term
+l_Ω(v) = ∫((u ⊗ c) ⊡ ∇(v))dΩ # Volumic convective term
 
-function upwind(ui, uj, ci, cj, nij)
-    cij = ci ⋅ nij
+function upwind(ui, uj, ci, cj, Pi, Pj, nij)
+    # "Projection" (norme conservée) de la vitesse de transport analytique
+    # et de la quantité uj dans le plan de la cellule i
+    _ci = normalize(Pi * ci) * norm(ci)
+    _uj = norm(uj) > 0 ? normalize(Pi * uj) * norm(uj) : uj
+    @show _uj
+
+    cij = _ci ⋅ nij
+    # cij = (0.5 * (ci + cj) / 2) ⋅ nij
     if cij > zero(cij)
         flux = cij * ui
     else
-        flux = cij * uj
+        flux = cij * _uj
     end
     flux
 end
-flux = upwind ∘ (side⁻(u), side⁺(u), side⁻(c), side⁺(c), side⁻(nΓ))
-l_Γ(v) = ∫(-flux * jump(v))dΓ
 
-# l(v) = l_Ω(v) # OK
-# l(v) = l_Γ(v) # OK
+function upwind_v2(ui, uj, ci, cj, Pi, Pj, vi, vj, nij)
+    # "Projection" (norme conservée) de la vitesse de transport analytique
+    # et de la quantité uj dans le plan de la cellule i
+    _ci = normalize(Pi * ci) * norm(ci)
+    _uj = norm(uj) > 0 ? normalize(Pi * uj) * norm(uj) : uj
+    @show _uj
+
+    cij = _ci ⋅ nij
+    if cij > zero(cij)
+        fi = cij * ui
+    else
+        fi = cij * _uj
+    end
+
+    fj = norm(fi) > 0 ? normalize(Pj * fi) * norm(fi) : fi
+
+    return fi ⋅ vi - fj ⋅ vj
+end
+
+function flux(v)
+    upwind_v2 ∘ (
+        side⁻(u),
+        side⁺(u),
+        side⁻(c),
+        side⁺(c),
+        side⁻(P),
+        side⁺(P),
+        side⁻(v),
+        side⁺(v),
+        side⁻(nΓ),
+    )
+end
+l_Γ(v) = ∫(-flux(v))dΓ
+
 l(v) = l_Ω(v) + l_Γ(v)
-
-#--- DBG
-cInfo = Bcube.CellInfo(mesh, 1)
-cPoint = Bcube.CellPoint(SA[0.0], cInfo, Bcube.ReferenceDomain())
-cshape = Bcube.shape(Bcube.celltype(cInfo))
-λ = Bcube.get_shape_functions(U, cshape)
-_λ = Bcube.blockmap_shape_functions(V, cInfo)
-
-a = Bcube.TangentialProjector()
-a = Bcube.materialize(a, cInfo)
-Bcube.show_lazy_operator(a)
-a = Bcube.materialize(a, cPoint)
-@show a
-#--- DBG
 
 # Time step
 dl = 2π * radius / nθ # analytic length
@@ -127,7 +159,11 @@ b = Bcube.allocate_dofs(U)
 for i in 1:nite
     b .= 0.0
     assemble_linear!(b, l, V)
+    @show b
+
     u.dofValues .+= Δt .* invM * b
+
+    @show u.dofValues
 
     ## Build animation
     plt = plot_solution(u, mesh, degree, xplot, yplot, xnodes, ynodes)
