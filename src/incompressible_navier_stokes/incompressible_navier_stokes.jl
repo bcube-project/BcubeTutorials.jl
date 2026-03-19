4e528a791428bc0f13f87ae6e6072d51b146db5d
bmxam <maxime.bouyges@gmaiL.com>
2026-03-19 20:24:09 +0100

    Fix tolerance (#43)

--- src/incompressible_navier_stokes/incompressible_navier_stokes.jl
+++ src/incompressible_navier_stokes/incompressible_navier_stokes.jl
@@ -308,7 +308,7 @@ function run_unsteady_mixed()
         test_ref(                               #src
             "$(prefix)_$(name)_velocity.jld2",  #src
             get_dof_values(velocity),           #src
-            compare(; rtol = 1e-6),             #src
+            compare(; rtol = 2e-5),             #src
         )                                       #src
         test_ref(                               #src
             "$(prefix)_$(name)_pressure.jld2",  #src
